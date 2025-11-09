1cac4e65deef248806f6408ffe5bc41b2326d5a7
Ebaad Raheem <143934811+ebaadraheem@users.noreply.github.com>
2025-11-09 11:11:38 +0500

    Update server.js

--- server.js
+++ server.js
@@ -30,7 +30,9 @@ app.get("/", (req, res) => {
   res.send("Recipe App server is running....");
 });
 
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`✅ Server running on port ${PORT}`);
 });
+
 
